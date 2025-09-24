47480374cdadf28af73f686b9ec17a0ebda8b1e4
Josh LeBlanc <joshcapitals07@gmail.com>
2025-09-24 16:32:18 -0400

    fixed syntax checker scripts no longer require a .env file

--- syntax-checker/get-log.sh
+++ syntax-checker/get-log.sh
@@ -4,18 +4,16 @@ if [ ! -e SYNTAX_CHECKER_ENV ]; then
     echo "Error: SYNTAX_CHECKER_ENV file not found"
     exit 1
 fi
-if [ ! -e ../.env ]; then
-    echo "Error: ../.env file not found"
-    exit 1
+if [ -z "${GH_TOKEN+x}" ]; then
+    if [ ! -e ../.env ]; then
+	echo "Error: GH_TOKEN and ../.env file not found"
+	exit 1
+    fi
+    source ../.env
 fi
 
 source SYNTAX_CHECKER_ENV
-source ../.env
 
-if [ -z "${GH_TOKEN+x}" ]; then
-    echo "Error: GH_TOKEN variable is not defined."
-    exit 1
-fi
 if [ -z "${GROUP_NUM+x}" ]; then
     echo "Error: GROUP_NUM variable is not defined."
     exit 1
